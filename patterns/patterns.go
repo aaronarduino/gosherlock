1f4f8eceb7fb638b1699ed1f0b116541c959611d
Aaron <aarona.arduino@gmail.com>
2017-04-26 22:26:09 -0500

    added more helpers

--- patterns/patterns.go
+++ patterns/patterns.go
@@ -3,4 +3,36 @@ package patterns
 const (
 	RangeSplitters = `(\bto\b|\-|\b(?:un)?till?\b|\bthrough\b|\bthru\b|\band\b|\bends?\b)`
 	EscapeRegex    = `[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]`
+	// oct, october
+	months = "\\b(jan(?:uary)?|feb(?:ruary)?|mar(?:ch)?|apr(?:il)?|may|jun(?:e)?|jul(?:y)?|aug(?:ust)?|sep(?:tember)?|oct(?:ober)?|nov(?:ember)?|dec(?:ember)?)\\b"
+	// 3, 31, 31st, fifth
+	days = "\\b(?:(?:(?:on )?the )(?=\\d\\d?(?:st|nd|rd|th)))?([1-2]\\d|3[0-1]|0?[1-9])(?:st|nd|rd|th)?(?:,|\\b)"
+	// 2014, 1990
+	// Does not recognize 1930 for example because that could be confused with a valid time.
+	// Exceptions are made for years in 21st century.
+	years = "\\b(20\\d{2}|\\d{2}[6-9]\\d)\\b"
+
+	// 5/12/2014
+	shortForm = `\b(0?[1-9]|1[0-2])\/([1-2]\d|3[0-1]|0?[1-9])\/?(\d{2,4})?\b`
+
+	// tue, tues, tuesday
+	weekdaysStr       = "\\b(sun|mon|tue(?:s)?|wed(?:nes)?|thu(?:rs?)?|fri|sat(?:ur)?)(?:day)?\\b"
+	relativeDateStr   = "((?:next|last|this) (?:week|month|year)|tom(?:orrow)?|tmrw|tod(?:ay)?|(?:right )?now|tonight|day after (?:tom(?:orrow)?|tmrw)|yest(?:erday)?|day before yest(?:erday)?)"
+	inRelativeDateStr = "(\\d{1,4}|a) (day|week|month|year)s? ?(ago|old)?"
+
+	inRelativeTime = `\b(\d{1,2} ?|a |an )(h(?:our)?|m(?:in(?:ute)?)?)s? ?(ago|old)?\b`
+	inMilliTime    = `\b(\d+) ?(s(?:ec(?:ond)?)?|ms|millisecond)s? ?(ago|old)?\b`
+	midtime        = `(?:@ ?)?\b(?:at )?(dawn|morn(?:ing)?|noon|afternoon|evening|night|midnight)\b`
+	// 23:50, 0700, 1900
+	internationalTime = `\b(?:(0[0-9]|1[3-9]|2[0-3]):?([0-5]\d))\b`
+	// 5, 12pm, 5:00, 5:00pm, at 5pm, @3a
+	explicitTime = `(?:@ ?)?\b(?:at |from )?(1[0-2]|[1-2]?[1-9])(?::?([0-5]\d))? ?([ap]\.?m?\.?)?(?:o'clock)?\b`
+
+	more_than_comparator = `((?:more|greater|older|newer) than|after|before)`
+	less_than_comparator = `((?:less|fewer) than)`
+
+	// filler words must be preceded with a space to count
+	fillerWords = ` (from|is|was|at|on|for|in|due(?! date)|(?:un)?till?)\b`
+	// less aggressive filler words regex to use when rangeSplitters are disabled
+	fillerWords2 = ` (was|is|due(?! date))\b`
 )
--- utils.go
+++ utils.go
@@ -1,10 +1,55 @@
 package gosherlock
 
 import (
+	"log"
 	"regexp"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/aaronarduino/gosherlock/patterns"
+)
+
+var (
+	monthToInt = map[string]int{"jan": 0, "feb": 1, "mar": 2, "apr": 3, "may": 4, "jun": 5, "jul": 6, "aug": 7, "sep": 8, "oct": 9, "nov": 10, "dec": 11}
+
+	// mapping of words to numbers
+	wordsToInt = map[string]int{
+		"one":     1,
+		"first":   1,
+		"two":     2,
+		"second":  2,
+		"three":   3,
+		"third":   3,
+		"four":    4,
+		"fourth":  4,
+		"five":    5,
+		"fifth":   5,
+		"six":     6,
+		"sixth":   6,
+		"seven":   7,
+		"seventh": 7,
+		"eight":   8,
+		"eighth":  8,
+		"nine":    9,
+		"ninth":   9,
+		"ten":     10,
+		"tenth":   10,
+	}
+
+	// mapping of number to words
+	intToWords = []string{
+		"one|first",
+		"two|second",
+		"three|third",
+		"four|fourth",
+		"five|fifth",
+		"six|sixth",
+		"seven|seventh",
+		"eight|eighth",
+		"nine|ninth",
+		"ten|tenth",
+	}
 )
 
 func MonthDiff(d1, d2 time.Time) int {
@@ -24,4 +69,38 @@ func EscapeRegExp(str string) string {
 
 func IsSameDay(date1, date2 time.Time) bool {
 	return date1.Month() == date2.Month()
+}
+
+// StrToNum converts all the words in a string
+// into numbers, such as four -> 4
+func StrToNum(str string) string {
+	tmp := regexp.MustCompile(``)
+	return tmp.ReplaceAllStringFunc(str, func(val string) string {
+		var out = strconv.Itoa(wordsToInt[val])
+		if strings.Index(val[len(val)-2:], "th") != -1 {
+			out += "th"
+		} else if strings.Index(val[len(val)-2:], "st") != -1 {
+			out += "st"
+		} else if strings.Index(val[len(val)-2:], "nd") != -1 {
+			out += "nd"
+		} else if strings.Index(val[len(val)-2:], "rd") != -1 {
+			out += "rd"
+		}
+		return out
+	})
+
+}
+
+// NumToStr converts all the numbers in a string
+// into regex for number|word, such as 4 -> 4|four
+func NumToStr(str string) string {
+	tmp := regexp.MustCompile(`((?:[1-9]|10)(?:st|nd|rd|th)?)`)
+	return tmp.ReplaceAllStringFunc(str, func(val string) string {
+		i, err := strconv.ParseInt("-42", 10, 64)
+		if err != nil {
+			log.Println(err)
+			return ""
+		}
+		return "(?:" + val + "|" + intToWords[i-1] + ")"
+	})
 }
